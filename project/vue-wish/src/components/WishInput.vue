9f8e80ab1d956de537725597a1f11bf985b8c3f9
ashram7 <ashram7@hanmail.net>
2021-12-22 15:09:16 +0900

    WishInput.vue 파일에 콤포넌트로 등록

--- project/vue-wish/src/components/WishInput.vue
+++ project/vue-wish/src/components/WishInput.vue
@@ -5,14 +5,27 @@
     <span class="addContainer" v-on:click="addWish">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+        <!-- 모달 제목 -->
+        <h3 slot="header">경고</h3>
+        <!-- 모달 내용 -->
+        <span slot="footer" @click="showModal = false">
+            위시 리스트를 입력하세요.
+            <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+        </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data(){
     return {
-      newWishItem:''
+      newWishItem:'',
+      showModal: false
     }
   },
   methods:{
@@ -24,12 +37,17 @@ export default {
         this.$emit('addWish',value);
         //localStorage.setItem(value, value);
         this.clearInput()
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput(){
       this.newWishItem = '';
     }
-  }
+  },
+  components: {
+      Modal: Modal
+  }  
 }
 </script>
 
