6dd4ff1f159aafab44fdab01f74900b9e82ad7ca
ashram7 <ashram7@hanmail.net>
2021-12-22 12:26:38 +0900

    위시리스트 목록 만들기

--- project/vue-wish/src/components/WishList.vue
+++ project/vue-wish/src/components/WishList.vue
@@ -1,10 +1,25 @@
 <template lang="html">
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="wishItem in wishItems"> {{ wishItem }} </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      wishItems: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.wishItems.push(localStorage.key(i));
+      }
+    }
+  }
 }
 </script>
 
